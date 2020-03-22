ec80298970d671e035a56e1cbb9c688b8c0f447c
Daniel Lwetabe <acehanks@gmail.com>
2020-03-22 23:41:13 +0300

    added HTTPClient for requests TODO: Make POST request

--- src/main/kotlin/restConsume/rest/Controller.kt
+++ src/main/kotlin/restConsume/rest/Controller.kt
@@ -1,11 +1,14 @@
 package restConsume.rest
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.client.RestTemplate
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
 
 @RestController
 @RequestMapping("/api")
@@ -17,10 +20,28 @@ class Controller() {
     @JsonIgnoreProperties(ignoreUnknown = true)
     data class Quote(var type : String = "", var value : Value? = null)
 
+    @GetMapping("/quotes")
+    fun getQuotes() {
+        val url: String = "http://gturnquist-quoters.cfapps.io/api/random"
+        val quotes = RestTemplate().getForObject(url, Quote::class.java)
+        println(quotes)
+    }
+
+    @GetMapping("/post")
+    fun getPost() {
+        val url: String = "https://jsonplaceholder.typicode.com/posts/1"
+        val post = RestTemplate().getForObject(url, Post::class.java)
+        println(post)
+    }
+
     @GetMapping("/posts")
     fun getPosts() {
-        val URL: String = "https://jsonplaceholder.typicode.com/posts"
-        val posts = RestTemplate().getForObject("http://gturnquist-quoters.cfapps.io/api/random", Quote::class.java)
-        println(posts)
+        val url = "https://jsonplaceholder.typicode.com/posts"
+        val client = HttpClient.newBuilder().build()
+        val request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .build();
+        val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+        println(response.body())
     }
 }
--- src/main/kotlin/restConsume/rest/Post.kt
+++ src/main/kotlin/restConsume/rest/Post.kt
@@ -5,3 +5,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class Post(val userId: Int, val id: Int, val title: String, val body: String) {
 }
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+class Posts(vararg post: Post)
